8edeabf0c12077242bba7d0734f19531ad4e32fb
swarupkshatriya <swarup.kshatriya@gmail.com>
2023-04-21 13:31:03 +0530

    added aditional check.

--- src/Testing/TestingClass3.java
+++ src/Testing/TestingClass3.java
@@ -2,6 +2,6 @@ package Testing;
 
 public class TestingClass3 {
 		public void testing3(){
-			System.out.println("Gello jaahnna");
+			System.out.println("Gello jaahnna Hello this is new code again");
 		}
 }
